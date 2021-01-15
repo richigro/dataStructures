68aadf6cc4e4a3d1054a5e22c4ce6b08c98514ad
Ricardo Guerrero <richi_gro@hotmail.com>
2021-01-14 19:29:49 -0600

    two implemetations of queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,72 @@
+const { _ } = require("../../../lib/underscore/underscore");
+
+var Node = function(val, next=null){
+  var someInstance = {};
+
+  someInstance.value = val;
+  someInstance.next = next;
+
+  return someInstance;
+}
+
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.sizeOfQueue = 0;
+  someInstance.head = null;
+  someInstance.tail = null;
+
+ _.extend(someInstance, Queue.queueMethods);
+
+  return someInstance;
 };
 
-var queueMethods = {};
+Queue.queueMethods = {
+  enqueue: function(value){
+    //create a node with the value
+    var node = new Node(value);
+    //check to see if queue is empty
+    if(!this.sizeOfQueue){
+      //assign the first node to be the head and 
+      // also the tail of the node 
+      this.tail = node;
+      this.head =node;
+    } else {
+      //if its not empty
+      //add from the tail
+      //The next pointer from the tail should point
+      // to the new node that will be a tail
+      this.tail.next = node;
+      //re-assign the new tail to be the new node
+      this.tail = node;
+    }
+    //increment the size of the queue by 1
+    this.sizeOfQueue++;
+
+  },
+  dequeue: function(){
+    //since we always remove from the head node 
+    // store it to then return it
+    var oldHead = this.head;
+
+    //if the current Queue is empty
+    if(!this.sizeOfQueue){
+      //then there are no more nodes to remove from queue
+      return undefined;
+    } else {
+      //if there are nodes change the current head 
+      //to the head's next pointer
+      this.head = this.head.next;
+    }
+    //decrement the size of the queue
+    this.sizeOfQueue--;
+    //return the value of the deleted node
+    return oldHead.value;
+  },
+  size: function(){
+    return this.sizeOfQueue;
+  }
+};
 
 
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,78 @@
+var Node = function(val, next=null){
+  var someInstance = {};
+
+  someInstance.value = val;
+  someInstance.next = next;
+
+  return someInstance;
+}
+
+//I will be impleting a Queue with a linked list
+//for better dequeing performance compared to an array
+
 var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
-
+  // var storage = {};
+  someInstance.head = null;
+  someInstance.tail = null;
+  someInstance.sizes = 0;
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    //instantiate a new node with the given value
+    var node = new Node(value);
+    //check to see if the linked list is empty
+    if(!someInstance.sizes){
+      //if empty set the node equal to the head and the tail
+      someInstance.head = node;
+      someInstance.tail = node;
+    } else {
+      //if the list is not empty 
+      //set the old tail to be pointing to the new 
+      //tail 
+      someInstance.tail.next = node;
+      //make the someInstance.tail pointer point
+      // to the new node
+      someInstance.tail = node;
+    }
+    //increment the size of the queue
+    someInstance.sizes++;
+    
   };
 
   someInstance.dequeue = function() {
+    var oldHead = someInstance.head;
+    //dequeueing from empty queue
+    if(!someInstance.sizes){
+      //if the queue is currently empty
+      console.log("There are no more nodes in the Queue to remove!");
+      return 0;
+    } else {
+      //we are always going to remove from the head
+      //keep a reference to of current head
+      
+      //make our someInstance.head pointer point 
+      // to the new head
+      someInstance.head = oldHead.next;
+    }
+    //decrease the count by 1
+    someInstance.sizes--;
+    //return the value of the node that was removed
+    return oldHead.value;
   };
 
   someInstance.size = function() {
+    return someInstance.sizes;
   };
 
   return someInstance;
 };
+
+var myQueue = Queue();
+
+myQueue.enqueue(5);
+myQueue.enqueue(6);
+myQueue.enqueue(9);
+myQueue.dequeue();
+console.log(myQueue);
